a7913581c675017b6903830407244c6a2ba3869c
AdamCargillVega <Adam@levett.biz>
2024-04-13 12:28:36 +0200

    Arm Object

---
+++ Assets/_Project/Scripts/Runtime/Player/Summoning/SummoningBehaviour.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SummoningBehaviour : MonoBehaviour
+{
+    internal enum ArmState
+    {
+        Idle,
+        State1,
+        State2,
+        State3,
+        State4,
+    }
+
+}
