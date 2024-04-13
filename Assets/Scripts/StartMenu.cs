83f03f5d0d188914ce25918669a7b2d67e5499a6
Max Schulte <maxgs1001@gmail.com>
2024-04-13 13:44:42 +0200

    Start, quit and reload
    Added buttons and function

--- Assets/Scripts/StartMenu.cs
+++ Assets/Scripts/StartMenu.cs
@@ -8,6 +8,6 @@ public class StartMenu : MonoBehaviour
     public void StartGame()
     {
         //loads the next scene which is level1
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
